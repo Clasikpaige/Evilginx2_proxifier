96901ba1e2e3856c20ca7c4a8c48eef176b99130
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-09-03 20:40:10 +0100

    Update proxify.py

--- proxify.py
+++ proxify.py
@@ -1,4 +1,5 @@
 import yaml
+import subprocess
 
 # Load the YAML configuration file
 with open('evilginx2_config.yaml', 'r') as config_file:
@@ -9,9 +10,15 @@ phishing_targets = config_data['phishing_targets']
 web_server = config_data['web_server']
 phishing_domain = config_data['phishing_domain']
 
-# Now you can use the configuration data as needed
+# Print the configuration data (you can remove this if not needed)
 print("Phishing Targets:")
 for target in phishing_targets:
     print(f"Domain: {target['domain']}, SSLStrip: {target['sslstrip']}")
 print(f"Web Server Address: {web_server['address']}, Port: {web_server['port']}")
 print(f"Phishing Domain: {phishing_domain}")
+
+# Assuming you have Evilginx2 installed, you can run it here with the generated config
+try:
+    subprocess.run(["evilginx", "-c", "evilginx2_config.yaml"], check=True)
+except subprocess.CalledProcessError as e:
+    print(f"Error running Evilginx2: {e}")
