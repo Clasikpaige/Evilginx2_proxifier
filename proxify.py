55c08e8dfc00c33e4429bcab46f85bfbaabb1842
Paige <102603571+Clasikpaige@users.noreply.github.com>
2023-12-24 07:54:06 +0100

    Update proxify.py

--- proxify.py
+++ proxify.py
@@ -6,16 +6,10 @@ with open('evilginx2_config.yaml', 'r') as config_file:
     config_data = yaml.load(config_file, Loader=yaml.FullLoader)
 
 # Access the configuration data
-phishing_targets = config_data['phishing_targets']
 web_server = config_data['web_server']
-phishing_domain = config_data['phishing_domain']
 
 # Print the configuration data (you can remove this if not needed)
-print("Phishing Targets:")
-for target in phishing_targets:
-    print(f"Domain: {target['domain']}, SSLStrip: {target['sslstrip']}")
 print(f"Web Server Address: {web_server['address']}, Port: {web_server['port']}")
-print(f"Phishing Domain: {phishing_domain}")
 
 # Assuming you have Evilginx2 installed, you can run it here with the generated config
 try:
